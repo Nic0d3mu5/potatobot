1f7bab9031f83f69a6211b5feed7b59bb16b6aa3
Nic0d3mu5 <58745733+Nic0d3mu5@users.noreply.github.com>
2019-12-10 21:00:27 +0100

    Update potatobot.js

--- .gitignore/potatobot.js
+++ .gitignore/potatobot.js
@@ -2,7 +2,7 @@ const discord = require ('discord.js');
 
 const client = new Discord.Client();
 
-client.login(process.env.TOKEN);
+client.login("NjU0MDI1NzY1MjQ2OTkyMzg2.Xe_j3w.sZHbSrVmU9JbpLr8gvUGdD3gYG4");
 
 var prefix = ("$");
 
